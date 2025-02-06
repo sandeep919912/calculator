ec304a9f9ee3fcd2d561958bd144b2b9be9aa0c9
Sandeep pandit <sandeeppandit919912@gmail.com>
2025-02-06 10:35:10 +0530

    calculator for my use

---
+++ jsCalc.js
@@ -0,0 +1,56 @@
+const buttons = document.querySelectorAll(".btns input");
+const inputText = document.getElementById("inputBox");
+const del = document.getElementById("del");
+
+buttons.forEach(button=>{
+    button.addEventListener("click", ()=>{
+        if(button.value === "="){
+            if(inputText.value == ""){
+                alert("You have to enter some number to perform calculations");
+            }else{
+                inputText.value = eval(inputText.value);
+            }
+        }
+        else if(button.value === "AC"){
+            inputText.value = "";
+        }
+        else if(button.value ==="DEL"){
+            let deleteOne = inputText.value.toString().slice(0,-1);
+            inputText.value = deleteOne;
+        }
+        else{
+            inputText.value += button.value
+        }
+    })
+})
+
+document.addEventListener("keypress",(e)=>{
+    if(e.key === "Enter"){
+        if(inputText.value == ""){
+            alert("You have to enter some number to perform calculations");
+        }else{
+            inputText.value = eval(inputText.value);
+        }
+    }
+});
+
+
+
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         if (button.value === "=") {
+//                 // Evaluate the expression and update the inputText value
+//                 inputText.value = eval(inputText.value);
+//         } else if (button.value === "AC") {
+//             inputText.value = "";
+//         }
+//         else if(button.value ==="DEL"){
+//             let deleteOne = inputText.value.toString().slice(0,-1);
+//             inputText.value = deleteOne
+//         }
+//         else {
+//             // Append the button value to the inputText
+//             inputText.value += button.value;
+//         }
+//     });
+// });
